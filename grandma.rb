70398ac818e528491870cad248fc74d35361dbee
Eric McKinney <mckinney99@gmail.com>
2017-10-19 18:15:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(grandma)
 		return "I LOVE YOU TOO PUMPKIN!"
 	elsif grandma == speak_to_grandma.upcase
 		return "NO, NOT SINCE 1938!"
-	else 
+	else
     puts "HUH?! SPEAK UP, SONNY!"
 end
 end
